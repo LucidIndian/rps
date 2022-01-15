1c8cee482c259da715f3495339a32fbc47ee39e4
Tygh Walters <tyghwalters@gmail.com>
2022-01-15 10:15:56 -0800

    Added my first .js file and linked it to the HTML file

---
+++ rps.js
@@ -0,0 +1,7 @@
+// this is where my RPS game will be built
+// it's linked in the HTML file
+// all the script will be here (vs the page)
+// so I can keep pages cleaner and have 
+// only 1 version of the code to maintain
+console.log("Hello World");
+
