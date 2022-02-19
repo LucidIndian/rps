3d8fb3ff16b26167ab4132b6a4d4162e4e44f66a
Tygh Walters <tyghwalters@gmail.com>
2022-02-19 14:10:07 -0800

    Added buttons and now displaying results in the DOM vs the console

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # rps
-This is my work to create a RPS game with JavaScript in the console. 
+This is my work to create a RPS (Rock, Paper, Scissors) game with JavaScript in the console. 
 
-
-Log: 
+Work log: 
 
 - On 1/19/22 I finished the game to where it plays 5 rounds and decalres a winner. Pretty basic, but pushing to GitHub and moving on to the next TOP lesson. iirc, I'll be coming back to the project after some more lessons. ttyl...
 
+- On 2/6/22 TOP has me coming back to the game to add a new feature (UI) in a branch, then merge it with main. 
+
+-- On 2/19/22 I came back to convert my console logging to a real UI by printing the results to the HTML page.
--- rps.js
+++ rps.js
@@ -1,18 +1,46 @@
-// This is where my RPS game will be built, linked in the HTML file
-// all the script will be here (vs the page) so I can keep pages cleaner and have only 1 version of the RPS code to maintain
+// README: This is where my RPS game will be built, linked in the index.html file of this same folder
+// All the script will be here (vs the page) so I can keep pages cleaner & have 1 version of the code to maintain
 
-// Global Variables
+// Global Variables:
 let playerSelection = "Pending"; // Added a string assignment and placed in global scope work to get out of "undefined
 let computerSelection = "Pending";
 let playerRoundsWon = 0; // initialized to their starting score
 let cpuRoundsWon = 0; // initialized to their starting score
 let result = "pending"; // created a variable to be re-assigned depeding on each round's outcome.
 let gameResult = "Pending"; // Added a string assignment and placed in global scope work to get out of "undefined. Article: https://developer.mozilla.org/en-US/docs/Glossary/Hoisting - "An exception will be thrown if a variable declared with let or const is read before it is initialized."
-let theWinner = "tbd"; // End game, assigned a string of the player with the highest score, or a tie
+let overallWinner = "tbd"; // End game, assigned a string of the player with the highest score, or a tie
 
+// Event Listner for all the buttons:
+// Create an array from the nodelist of buttons on the HTML page:
+const buttons = Array.from(document.querySelectorAll('.button')); 
+// Add the E.L. to each button so we know when each is clicked:
+buttons.forEach(button => button.addEventListener('click', playerPick));
 
-// FUNCTION A: computerPlay 
-// A fxn called "computerPlay" - randomly return a string: ‘Rock’, ‘Paper’ or ‘Scissors’, called in the `game` function in each round. 
+// FUNCTION: PlayerPick
+// Called by the Event Listener to assign a new Player pick. 
+function playerPick(e) {
+  playerSelection = e.target.id; // this grabs the HTML ID of the button ("rock", "paper", or "scissors"), aka, the players choice, and assigns it to the playerSelection variable.
+  // "Add a div for displaying results (Did that on the .html) + change all console.logs into DOM methods.":
+  const container1 = document.querySelector('#rpsResults'); // selects the target element of the DOM (where we want to manipulate)
+  const resultsText = document.createElement('p'); // creates the p element in memory
+  resultsText.textContent =  "The player selects " + playerSelection; // adds text to the p element in memory
+  resultsText.style.color = 'blue'; // styles text to the p element in memory
+  rpsResults.appendChild(resultsText); // adds the p element to the DOM (prints the playerSelection)
+  computerSelection = computerPlay(); // Next, we ask the CPU to make a new choice. The value of the computerPlay fxn is assigned to this variable 'computerSelection'
+  const container2 = document.querySelector('#rpsResults'); // selects the target element of the DOM (where we want to print the CPU selection)
+  const resultsText2 = document.createElement('p'); // creates the p element in memory
+  resultsText2.textContent =  "The CPU selects " + computerSelection; // adds text to the p element in memory
+  resultsText2.style.color = 'red'; // styles text to the p element in memory
+  rpsResults.appendChild(resultsText2); // adds the p element to the DOM (prints the playerSelection)
+  // trigger the playRound fxn with parameters and assign value to a variable. 
+  // FYI: When parameters are excluded, it evaluates to the "else" option! (Took 30 min to troubleshoot this)  
+  gameResult = playRound(playerSelection, computerSelection); // play a round with the new selections, need to include the parameters, otherwise it uses the initial values.
+  addScore();  // after we have the result, we tally the score
+} // end of the playerPick fxn
+
+// FUNCTION: computerPlay 
+// Called by the playRound function to assign a new CPU guess. 
+// It randomly return string: ‘Rock’, ‘Paper’ or ‘Scissors’, called in the `game` function in each round. 
 function computerPlay() {
   randomNumber = Math.random(); // doens't need `let` so it's not hoisted and randomNumber is declared and initialized before it's used.
   if (randomNumber > 0.66) { // ~top 33% of the random #s
@@ -25,7 +53,58 @@ function computerPlay() {
 } //end of the computerPlay fxn
 
 
-// FUNCTION D: addScore 
+// FUNCTION: playRound
+  // Play a single round of RPS. Take 2 params "playerSelection" and "computerSelection"
+  // "playerSelection" should be from the button.
+  // return a string declaring the winner of the round
+
+  function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("The computer wins!");
+        result = "The computer wins!" // I think I have to create a variable because I'm getting "gameResult" as undefined
+        return result;
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
+        console.log("It's a tie!");
+        result = "It's a tie!";
+        return result;
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("The player wins!");
+        result = "The player wins!";
+        return result;
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        console.log("The computer wins!");
+        result = "The computer wins!";
+        return result;
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        console.log("It's a tie!");
+        result = "It's a tie!";
+        return result;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("The player wins!");
+        result = "The player wins!";
+        return result;
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log("The computer wins!");
+        result = "The computer wins!";
+        return result;
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        console.log("It's a tie!");
+        result = "It's a tie!";
+        return result;
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("The player wins!");
+        result = "The player wins!";
+        return result;
+    } else {
+        console.log("Not a valid choice!");
+        result = "Not a valid choice!";
+        return result;
+    }
+} //end of the playRound fxn
+  
+
+// FUNCTION: addScore 
 function addScore() {  // helper function: add +1 to the winner after each round
   if (gameResult == "The computer wins!") {
     cpuRoundsWon = cpuRoundsWon + 1;
@@ -34,141 +113,42 @@ function addScore() {  // helper function: add +1 to the winner after each round
   } else {
     console.log("No points awarded");
   }
+  console.log("Round score: Player: "+ playerRoundsWon + ", " + "CPU: "+cpuRoundsWon); // moved this inside the bracket and now it returns the updated score and not the initial values of 0.
+
+  // Display the running score
+  const container3 = document.querySelector('#rpsResults'); // selects the target element of the DOM (where we want to manipulate)
+  const resultsText3 = document.createElement('p'); // creates the p element in memory
+  resultsText3.textContent =  "Round result: " + gameResult + " Player points: "+ playerRoundsWon + ", " + "CPU points: "+cpuRoundsWon; // adds text to the p element in memory
+  resultsText3.style.color = 'green'; // styles text to the p element in memory
+  resultsText3.style.fontSize = "small"; // styles text to the p element in memory
+  rpsResults.appendChild(resultsText3); // adds the p element to the DOM (prints the playerSelection)
+  finalWinner() // checks for a final winner
 } //end of the addScore fxn
 
 
-// FUNCTION C: game
-// Use the previous function inside of this one to play a 5-round game that keeps score:
-function game() {
+// Announce an "overallWinner" of the game once one player reaches 5 points.
 
-  // FUNCTION B: playRound
-  // Play a single round of RPS. Take 2 params "playerSelection" and "computerSelection"
-  // "playerSelection" should be case insensitive & defined by `prompt()`. Syntax: result = window.prompt(message, default);
-  // return a string declaring the winner of the round
-
-  function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        // console.log("The computer wins!");
-        result = "The computer wins!" // I think I have to create a variable because I'm getting "gameResult" as undefined
-        return result;
-    } else if (playerSelection == "rock" && computerSelection == "rock") {
-        // console.log("It's a tie!");
-        result = "It's a tie!";
-        return result;
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        // console.log("The player wins!");
-        result = "The player wins!";
-        return result;
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        // console.log("The computer wins!");
-         result = "The computer wins!";
-        return result;
-    } else if (playerSelection == "paper" && computerSelection == "paper") {
-        // console.log("It's a tie!");
-         result = "It's a tie!";
-        return result;
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        // console.log("The player wins!");
-         result = "The player wins!";
-        return result;
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        // console.log("The computer wins!");
-         result = "The computer wins!";
-        return result;
-    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        // console.log("It's a tie!");
-         result = "It's a tie!";
-        return result;
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        // console.log("The player wins!");
-        result = "The player wins!";
-        return result;
-    } else {
-        // console.log("Not a valid choice!");
-         result = "Not a valid choice!";
-        return result;
-    }
-  } //end of the playRound fxn
+function finalWinner() {
   
+  if (cpuRoundsWon > 4) {
+    console.log("Game over");
+    overallWinner = "The CPU wins the game!";
+  } else if (playerRoundsWon > 4) {
+    console.log("Game over");
+    overallWinner = "The Player wins the game!";
+  } else {
+  console.log("continue");
+  }
 
-  // Round 1: Run the 1-round fxn then console.log() the results 
-    // trigger the playRound fxn with parameters and assign value to a variable. 
-    // FYI: When parameters are excluded, it evaluates to the "else" option! (Took 30 min to troubleshoot this)
-    playerSelection = prompt().toLowerCase(); // player types a new choice put this inside the fxn so it's run whenever it's
-    console.log("Player selection for round 1 is "+ playerSelection);
+  if (overallWinner == "The CPU wins the game!" || overallWinner == "The Player wins the game!") {
+    const container4 = document.querySelector('#rpsResults'); // selects the target element of the DOM (where we want to manipulate)
+    const resultsText4 = document.createElement('h2'); // creates the p element in memory
+    resultsText4.textContent = "GAME OVER..." + overallWinner; // adds text to the p element in memory
+    resultsText4.style.color = 'black'; // styles text to the p element in memory
+    rpsResults.appendChild(resultsText4); // adds the p element to the DOM (prints the playerSelection)
+  } else {
+    // do nothing
+  };
 
-    computerSelection = computerPlay(); // CPU chooses a new choice. The value of the computerPlay fxn is assigned to this variable 'computerSelection'
-    console.log("CPU selection for round 1 is "+ computerSelection);
+} // finalWinner fxn end
 
-    gameResult = playRound(playerSelection, computerSelection); // huge win! This variable was "undefined" until I used the 'return' for the string in the upstream playRound function! Before that, it did not return anything (undefined), I was only console loggin it. BIG win. 
-    console.log("Game 1's result is: " + gameResult);
-
-    addScore();
-    console.log("Round 1 score: Player: "+ playerRoundsWon + ", " + "CPU: "+cpuRoundsWon); // moved this inside the bracket and now it returns the updated score and not the initial values of 0.
-
-  // Round 2: Run the 1-round fxn, add +1 to the winner, then console.log() the results 
-    playerSelection = prompt().toLowerCase(); // player types a new choice put this inside the fxn so it's run whenever it's
-    console.log("Player selection for round 2 is "+ playerSelection);
-
-    computerSelection = computerPlay(); // CPU chooses a new choice
-    console.log("CPU selection for round 2 is "+ computerSelection);
-
-    gameResult = playRound(playerSelection, computerSelection); // huge win! This variable was "undefined" until I used the 'return' for the string in the upstream playRound function! Before that, it did not return anything (undefined), I was only console loggin it. BIG win. 
-    console.log("Game 2's result is: " + gameResult);
-
-    addScore();
-    console.log("Round 2 score: Player: "+ playerRoundsWon + ", " + "CPU: "+cpuRoundsWon); // moved this inside the bracket and now it returns the updated score and not the initial values of 0.
-
-    // Round 3: Run the 1-round fxn, add +1 to the winner, then console.log() the results 
-    playerSelection = prompt().toLowerCase(); // player types a new choice put this inside the fxn so it's run whenever it's
-    console.log("Player selection for round 3 is "+ playerSelection);
-
-    computerSelection = computerPlay(); // CPU chooses a new choice
-    console.log("CPU selection for round 3 is "+ computerSelection);
-
-    gameResult = playRound(playerSelection, computerSelection); // huge win! This variable was "undefined" until I used the 'return' for the string in the upstream playRound function! Before that, it did not return anything (undefined), I was only console loggin it. BIG win. 
-    console.log("Game 3's result is: " + gameResult);
-
-    addScore();
-    console.log("Round 3 score: Player: "+ playerRoundsWon + ", " + "CPU: "+cpuRoundsWon); // moved this inside the bracket and now it returns the updated score and not the initial values of 0.
-
-    // Round 4: Run the 1-round fxn, add +1 to the winner, then console.log() the results 
-    playerSelection = prompt().toLowerCase(); // player types a new choice put this inside the fxn so it's run whenever it's
-    console.log("Player selection for round 4 is "+ playerSelection);
-
-    computerSelection = computerPlay(); // CPU chooses a new choice
-    console.log("CPU selection for round 4 is "+ computerSelection);
-
-    gameResult = playRound(playerSelection, computerSelection); // huge win! This variable was "undefined" until I used the 'return' for the string in the upstream playRound function! Before that, it did not return anything (undefined), I was only console loggin it. BIG win. 
-    console.log("Game 4's result is: " + gameResult);
-
-    addScore();
-    console.log("Round 4 score: Player: "+ playerRoundsWon + ", " + "CPU: "+cpuRoundsWon); // moved this inside the bracket and now it returns the updated score and not the initial values of 0.
-
-    // Round 5: Run the 1-round fxn, add +1 to the winner, then console.log() the results
-    playerSelection = prompt().toLowerCase(); // player types a new choice put this inside the fxn so it's run whenever it's
-    console.log("Player selection for round 5 is "+ playerSelection);
-
-    computerSelection = computerPlay(); // CPU chooses a new choice
-    console.log("CPU selection for round 5 is "+ computerSelection);
-
-    gameResult = playRound(playerSelection, computerSelection); // huge win! This variable was "undefined" until I used the 'return' for the string in the upstream playRound function! Before that, it did not return anything (undefined), I was only console loggin it. BIG win. 
-    console.log("Game 5's result is: " + gameResult);
-
-    addScore();
-    console.log("Round 5 score: Player: "+ playerRoundsWon + ", " + "CPU: "+cpuRoundsWon); // moved this inside the bracket and now it returns the updated score and not the initial values of 0.
-
-    if (playerRoundsWon > cpuRoundsWon) {
-      theWinner = "The PLayer wins";
-    } else if (cpuRoundsWon > playerRoundsWon) {
-      theWinner = "The CPU wins";
-    } else {
-      theWinner = "It's an overall tie! lol";
-    }
-
-  console.log(theWinner); // Return and console.log() the winner at the end (highest number of wins) 
-  return theWinner;
-
-} // this is the end of the game fxn
-game();
